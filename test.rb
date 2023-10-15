88a9d9a1d2673785bce876a9cf525d98d16ce471
kabetani-yusei <31913@toyota.kosen-ac.jp>
2023-10-15 14:04:38 +0900

    譲渡の追加

--- test.rb
+++ test.rb
@@ -1,5 +1,6 @@
 $stdout.sync = true
 N, D, Q = gets.split.map(&:to_i)
+ddd = D
 question_time = 0
 list = Array.new(D) { Array.new() }
 
@@ -41,20 +42,44 @@ for i in 1...D
   list_sort << i if l == i
 end
 
-
 # 交換と譲渡を繰り返して良くしていく
-dis2flag = 1
+dis2flag = 0
 list_copy = [[]]
 catch(:break_all) do
   while true
-    # 交換
+    while(list[list_sort[-1]].size == 1)
+      list_sort.pop
+      D -= 1
+    end
+
+
+
+    which = rand(0...6)
     list_copy = list.map(&:dup)
-    lower_side = list[list_sort[0]][rand(list[list_sort[0]].size)]
-    upper_side = list[list_sort[-1]][rand(list[list_sort[-1]].size)]
-    list[list_sort[0]].delete(lower_side)
-    list[list_sort[-1]].delete(upper_side)
-    list[list_sort[0]] << upper_side
-    list[list_sort[-1]] << lower_side
+    if which <= 3
+      # 交換
+      lower_side = list[list_sort[0]][rand(list[list_sort[0]].size)]
+      upper_side = list[list_sort[-1]][rand(list[list_sort[-1]].size)]
+      list[list_sort[0]].delete(lower_side)
+      list[list_sort[-1]].delete(upper_side)
+      list[list_sort[0]] << upper_side
+      list[list_sort[-1]] << lower_side
+    else
+      # 譲渡
+      upper_side = list[list_sort[-1]][rand(list[list_sort[-1]].size)]
+      list[list_sort[-1]].delete(upper_side)
+      # 失敗check
+      throw :break_all if question_time == Q
+      puts "#{list[list_sort[0]].size} #{list[list_sort[-1]].size} #{list[list_sort[0]].join(" ")} #{list[list_sort[-1]].join(" ")}"
+      s = gets.chomp
+      question_time += 1
+      if s != "<"
+        list = list_copy.map(&:dup)
+        next
+      end
+      list[list_sort[0]] << upper_side
+    end
+
     # 最初のやつのcheck
     list_sort_first = list_sort[0]
     list_sort_end = list_sort[D - 1]
@@ -135,10 +160,8 @@ catch(:break_all) do
   end
 end
 
-
-
-if dis2flag == 1
-  list_copy = list.map(&:dup)
+list_copy = list.map(&:dup) if dis2flag == 1
+if question_time != Q
   while(true)
     break if question_time == Q
     puts "1 1 0 1"
@@ -147,10 +170,9 @@ if dis2flag == 1
   end
 end
 
-
 ans = []
 for i in 0...N
-  for j in 0...D
+  for j in 0...ddd
     ans << j if list_copy[j].include?(i)
   end
 end
