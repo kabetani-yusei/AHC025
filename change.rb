324dc9e09bc3ff141f1da7ffa732d924d95b6ae0
kabetani-yusei <31913@toyota.kosen-ac.jp>
2023-10-20 18:22:48 +0900

    トポロジカルソート実装

--- change.rb
+++ change.rb
@@ -1,60 +1,15 @@
-#inフォルダーにあるやつ　->　out フォルダーに変える
-score_list = Array.new(101,0)
-for file_num in 0..99
-file_name = "%04d" % file_num
-lines = File.readlines("./in/" + file_name + ".txt")
-N, D, Q = lines[0].split.map &:to_i
-$AC_DATA = lines[1].split.map &:to_i
-#s = accheckみたいな感じで使う
-def ac_check(left, right)
-  l = 0
-  r = 0
-  left.each{|ll|l += $AC_DATA[ll]}
-  right.each{|rr|r += $AC_DATA[rr]}
-  if l == r
-    return "="
-  elsif l > r
-    return ">"
-  else
-    return "<"
-  end
-end
-output_file = []
-output_file_index = -1
-#1. N,D,Q = の行を消す
-#2. put s =  -> out_string = に変える
-#3. file.put sのところを元に戻す
-
-#ここからは手入力
-#3. getsをac_check(left, right)に変える
-#4. getsの次の行 output_file[output_file_index+=1] = out_string
-def comment_out()#list_copy = listのとこに追加（譲渡のとこに追加しないように注意)
-s = ac_check(left, right)
-output_file[output_file_index+=1] = out_string
-
-ans = []
-for check_i in 0...N
-  for check_j in 0...ddd
-    ans << check_j if list[check_j].include?(check_i)
-  end
-end
-out_string = "#c #{ans.join(" ")}"
-output_file[output_file_index+=1] = out_string
-end
-
-
-#
-#
-#ここから先のコードを変更する
-#
-#
-
 $stdout.sync = true
+N, D, Q = gets.split.map(&:to_i)
 ddd = D
 question_time = 0
 list = Array.new(D) { Array.new() }
 repeated_question = Hash.new()
 
+#トポロジカルソートっぽく1対1の既存質問を避ける
+#重みもつける->=なら0で、
+$up_to_down_tree = Array.new(N){Array.new(N, -1)}
+$dist = Array.new(N,0)
+$single_known = -1
 #トポロジカルソートっぽく1対1の既存質問を避ける
 #重みもつける->=なら0で、
 $up_to_down_tree = Array.new(N){Array.new(N, -1)}
@@ -124,9 +79,8 @@ for i in 1...D
   while (r - l) > 0
     c = (l + r) / 2
     question_content = "#{list[i].size} #{list[list_sort[c]].size} #{list[i].join(" ")} #{list[list_sort[c]].join(" ")}"
-    out_string = question_content
-    s = ac_check(list[i], list[list_sort[c]])
-    output_file[output_file_index+=1] = out_string
+    puts question_content
+    s = gets.chomp
     repeated_question = repeated_check(s,repeated_question,list[i],list[list_sort[c]])
     question_time += 1
 
@@ -160,15 +114,6 @@ break_time = 0
 question_time_copy = question_time
 catch(:break_all) do
   while true
-    ans = []
-    for check_i in 0...N
-      for check_j in 0...ddd
-        ans << check_j if list[check_j].include?(check_i)
-      end
-    end
-    out_string = "#c #{ans.join(" ")}"
-    output_file[output_file_index+=1] = out_string
-
     while(list[list_sort[-1]].size == 1)
       list_sort.pop
       D -= 1
@@ -183,8 +128,10 @@ catch(:break_all) do
     question_time_copy = question_time
     break if D == 1
 
+
     which = rand(4)
     list_copy = list.map(&:dup)
+    change_flag = 0
     if which <= 2
       # 交換
       next if list[list_sort[0]].size <= 1 || list[list_sort[-1]].size <= 1
@@ -194,9 +141,8 @@ catch(:break_all) do
       single_check = single_known_check(lower_side,upper_side,N)
       if single_check == nil
         throw :break_all if question_time == Q
-        out_string = question_content
-        s = ac_check([lower_side], [upper_side])
-        output_file[output_file_index+=1] = out_string
+        puts question_content
+        s = gets.chomp
         repeated_question = repeated_check(s, repeated_question, [lower_side], [upper_side])
         question_time += 1
         if s == "="
@@ -218,9 +164,8 @@ catch(:break_all) do
       question_content = "#{list[list_sort[0]].size} #{list[list_sort[-1]].size} #{list[list_sort[0]].join(" ")} #{list[list_sort[-1]].join(" ")}"
       if repeated_question[question_content] == nil
         throw :break_all if question_time == Q
-        out_string = question_content
-        s = ac_check(list[list_sort[0]], list[list_sort[-1]])
-        output_file[output_file_index+=1] = out_string
+        puts question_content
+        s = gets.chomp
         question_time += 1
         repeated_question = repeated_check(s,repeated_question,list[list_sort[0]],list[list_sort[-1]])
       else
@@ -243,9 +188,8 @@ catch(:break_all) do
       question_content = "#{list[list_sort[0]].size} #{list[list_sort[-1]].size} #{list[list_sort[0]].join(" ")} #{list[list_sort[-1]].join(" ")}"
       if repeated_question[question_content] == nil
         throw :break_all if question_time == Q
-        out_string = question_content
-        s = ac_check(list[list_sort[0]], list[list_sort[-1]])
-        output_file[output_file_index+=1] = out_string
+        puts question_content
+        s = gets.chomp
         question_time += 1
         repeated_question = repeated_check(s,repeated_question,list[list_sort[0]],list[list_sort[-1]])
       else
@@ -281,9 +225,8 @@ catch(:break_all) do
         question_content = "#{list[insert].size} #{list[list_sort[c]].size} #{list[insert].join(" ")} #{list[list_sort[c]].join(" ")}"
         if repeated_question[question_content] == nil
           throw :break_all if question_time == Q
-          out_string = question_content
-          s = ac_check(list[insert], list[list_sort[c]])
-          output_file[output_file_index+=1] = out_string
+          puts question_content
+          s = gets.chomp
           question_time += 1
           repeated_question = repeated_check(s,repeated_question,list[insert],list[list_sort[c]])
         else
@@ -336,39 +279,17 @@ list_copy = list.map(&:dup) if dis2flag == 1
 if question_time != Q
   while(true)
     break if question_time == Q
-    out_string = "1 1 0 1"
-    output_file[output_file_index+=1] = out_string
+    puts "1 1 0 1"
+    s = gets
     question_time += 1
   end
 end
 
-
-
-
-D = ddd
 ans = []
-score = Array.new(D){Array.new()}
 for i in 0...N
-  for j in 0...D
+  for j in 0...ddd
     ans << j if list_copy[j].include?(i)
-    score[j] << $AC_DATA[i] if list_copy[j].include?(i)
   end
 end
 
-heikin = $AC_DATA.sum / D
-bunsan = 0
-score.each{|ss_score|bunsan += (ss_score.sum-heikin)**2}
-score_list[file_num+1] = bunsan
-
-
-out_string = ans.join(" ")
-output_file[output_file_index += 1] = out_string
-file = File.open("./out/" + file_name + ".txt", "w")
-output_file.each{|dd|file.puts dd}
-file.close
-end
-
-score_list[0] = score_list.sum
-file = File.open("score.txt", "w")
-score_list.each{|dd|file.puts dd}
-file.close
+puts ans.join(" ")
